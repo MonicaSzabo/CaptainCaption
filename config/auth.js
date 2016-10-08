6dc7b6c4c0544a865c050cbb48ee30d2ea07b520
Monica Szabo <monicaszb@gmail.com>
2016-10-07 21:50:17 -0500

    fixed heroku link

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '772579238052-chia48uigq0823sadcg78ejofqqgg7sj.apps.googleusercontent.com',
         'clientSecret'  : 'SKtfriC46tTaK2HbgIzsdJ0_',
-        'callbackURL'   : 'http://localhost:3000/auth/google/callback'
+        'callbackURL'   : 'https://captaincaption.herokuapp.com/auth/google/callback'
     }
 
 };
