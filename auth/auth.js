0c9b46b0604c0a13f0cf7db6b6308f22b1290d91
Monica Szabo <monicaszb@gmail.com>
2017-10-02 11:52:10 -0500

    working on security

---
+++ auth/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    'googleAuth' : {
+        'clientID'      : '772579238052-chia48uigq0823sadcg78ejofqqgg7sj.apps.googleusercontent.com',
+        'clientSecret'  : 'SKtfriC46tTaK2HbgIzsdJ0_',
+        'callbackURL'   : 'https://captaincaption.herokuapp.com/auth/google/callback'
+    }
+
+};
